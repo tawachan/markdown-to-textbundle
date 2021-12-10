2601627862996913a5987d1fe736b5312005caae
tawachan <tawachan39@gmail.com>
2021-12-10 22:06:19 +0900

    v.0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # markdown-to-textbundle
+
+Markdown から Textbundle に変換する。
+
+# 使い方
+
+1. inputs フォルダに変換したい Markdown ファイルと画像ファイル等を全て入れる
+2. `templates/info.json` をカスタマイズする
+3. `node index.js` を実行する
+4. outpus フォルダに出力される
---
+++ index.js
@@ -0,0 +1,48 @@
+import fs from "fs";
+import path from "path";
+
+const OUTPUTS_PATH = "./outputs";
+const INPUTS_PATH = "./inputs";
+const TEMPLATES_PATH = "./templates";
+
+const main = async () => {
+  if (fs.existsSync(OUTPUTS_PATH)) {
+    fs.rmSync(OUTPUTS_PATH, { force: true, recursive: true });
+  }
+  fs.mkdirSync(OUTPUTS_PATH);
+
+  const fileNames = fs.readdirSync(INPUTS_PATH);
+  const markdownFileNames = fileNames.filter((n) => n.includes(".md"));
+
+  markdownFileNames.forEach((fileName) => {
+    const filePath = path.join(INPUTS_PATH, fileName);
+    const text = fs.readFileSync(filePath).toString();
+
+    const assetLinks = [
+      ...text.matchAll(/!\[(.+?\.(jpg|png|jpeg))\]\(.+?\.(png|jpeg|jpg)\)/g),
+    ].map((r) => r[1]);
+
+    const outputFolderPath = path.join(
+      OUTPUTS_PATH,
+      fileName.replace(".md", "")
+    );
+    fs.mkdirSync(outputFolderPath);
+
+    const inputInfoFilePath = path.join(TEMPLATES_PATH, "info.json");
+    const outputInfoFilePath = path.join(outputFolderPath, "info.json");
+    fs.copyFileSync(inputInfoFilePath, outputInfoFilePath);
+
+    const outPutMarkdownFilePath = path.join(outputFolderPath, fileName);
+    fs.copyFileSync(filePath, outPutMarkdownFilePath);
+
+    if (assetLinks.length > 0) {
+      assetLinks.forEach((link) => {
+        const inputAssetPath = path.join(INPUTS_PATH, link);
+        const outputAssetPath = path.join(outputFolderPath, link);
+        fs.copyFileSync(inputAssetPath, outputAssetPath);
+      });
+    }
+  });
+};
+
+main();
