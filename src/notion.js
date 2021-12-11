c2e8a663fb30a5db499ab2808041351732c09685
tawachan <tawachan39@gmail.com>
2021-12-12 02:45:12 +0900

    remove notion.js

--- src/notion.js
+++
@@ -1,6 +0,0 @@
-export const removeUniqueIdFromFileName = (fileName) => {
-  // Notionは "〈物語〉シリーズ セカンドシーズン b537c73e8449496e9d4bbd7c8c570922" のように最後にIDが付いた名称になるのでこれを取り除く
-  const fileNameArray = fileName.split(" ");
-  fileNameArray.pop();
-  return fileNameArray.join(" ");
-};
